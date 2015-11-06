82fd43d32497a09b8a11cea4306bd74fe899bc0f
arwen7stars <claudia.m.r.marinho@gmail.com>
2015-11-06 15:48:32 +0000

    V2.1
    -load mudado

--- src/main.cpp
+++ src/main.cpp
@@ -12,74 +12,6 @@
 #include "Desporto.h"
 #include "Data.h"
 #include "Campeonato.h"
-bool load(Campeonato c)
-{
-	bool suc_des;
-	string desportos = "C:\\Users\\Cláudia Marinho\\Desktop\\Projeto-Aeda\\src\\Desportos.txt";
-	suc_des = c.criaDesportosCampeonato(desportos);
-
-	if(suc_des)
-		return true;
-	else return false;
-
-	string modalidades = "C:\\Users\\Cláudia Marinho\\Desktop\\Projeto-Aeda\\src\\Modalidades.txt";
-	ifstream in_mod;
-	vector<Desporto*> d = c.getDesportos();
-
-	in_mod.open(modalidades.c_str());
-
-	while (!in_mod.eof())
-	{
-		char barra;
-		string desp = "";
-		string mod = "";
-		int horas;
-		int minutos;
-		int indice = -1;
-
-		string extraido = "";
-
-		if (!in_mod.eof())
-			do
-			{
-				in_mod >> extraido;
-				if (extraido != "/")
-					desp = desp + extraido;
-			} while (extraido != "/");
-
-		for (unsigned int i = 0; i < d.size(); i++)
-			if (d[i]->getNome() == desp)
-				indice = i;
-
-		if (indice == -1)
-			return false;
-
-		extraido = "";
-
-		if (!in_mod.eof())
-			do
-			{
-				in_mod >> extraido;
-				if (extraido != "/")
-					mod = mod + extraido;
-			} while (extraido != "/");
-
-		in_mod >> horas;
-		in_mod >> barra;
-		in_mod >> minutos;
-		if (!in_mod.eof())
-		{
-			Modalidade * m = new Modalidade(mod, horas, minutos, d[indice]);
-			d[indice]->adicionaModalidade(m);
-		}
-	}
-
-	return true;
-
-}
-
-
-void menuNovoCampeonato(){}
 
 Campeonato loadCampeonato(){
 	ifstream in;
@@ -139,6 +71,100 @@ Campeonato loadCampeonato(){
 	return c;
 }
 
+bool loadModalidades(Campeonato c)
+{
+	string modalidades = "C:\\Users\\Cláudia Marinho\\Desktop\\Projeto-Aeda\\src\\Modalidades.txt";
+	ifstream in_mod;
+	vector<Desporto*> d = c.getDesportos();
+
+	if(!ficheiroExiste(modalidades))
+		return false;
+
+	in_mod.open(modalidades.c_str());
+
+	while (!in_mod.eof())
+	{
+		char barra;
+		string desp = "";
+		string mod = "";
+		int horas;
+		int minutos;
+		int indice = -1;
+
+		string extraido = "";
+
+		if (!in_mod.eof())
+			do
+			{
+				in_mod >> extraido;
+				if (extraido != "/")
+					desp = desp + extraido;
+			} while (extraido != "/");
+
+		for (unsigned int i = 0; i < d.size(); i++)
+			if (d[i]->getNome() == desp)
+				indice = i;
+
+		if (indice == -1)
+			return false;
+
+		extraido = "";
+
+		if (!in_mod.eof())
+			do
+			{
+				in_mod >> extraido;
+				if (extraido != "/")
+					mod = mod + extraido;
+			} while (extraido != "/");
+
+		in_mod >> horas;
+		in_mod >> barra;
+		in_mod >> minutos;
+		if (!in_mod.eof())
+		{
+			Modalidade * m = new Modalidade(mod, horas, minutos, d[indice]);
+			d[indice]->adicionaModalidade(m);
+		}
+	}
+	return true;
+}
+
+
+bool load()
+{
+	Campeonato c = loadCampeonato();
+
+	bool suc_des;
+	string desportos = "C:\\Users\\Cláudia Marinho\\Desktop\\Projeto-Aeda\\src\\Desportos.txt";
+	suc_des = c.criaDesportosCampeonato(desportos);
+
+	if(suc_des)
+		return true;
+	else return false;
+
+	bool suc_eq;
+	string equipas = "C:\\Users\\Cláudia Marinho\\Desktop\\Projeto-Aeda\\src\\Atletas.txt";
+	suc_eq = c.criaEquipasCampeonato(equipas);
+
+	if(suc_eq)
+		return true;
+	else return false;
+
+	bool suc_mod;
+	suc_mod = loadModalidades(c);
+
+	if(suc_mod)
+		return true;
+	else return false;
+
+	return true;
+
+}
+
+
+void menuNovoCampeonato(){}
+
 void menu(){
 	vector<string> choices;
 	choices.push_back("Novo Campeonato");
@@ -177,17 +203,19 @@ int main(){
 //	cout << M.getNome() << endl;
 //	cout << M.getDesporto()->getNome() << endl;
 //	cout << endl;
-/*
+
 	Data d1 (2015,11,1);
 	Data d2 (2015,11,10);
+	Hora h1(8,0);
+	Hora h2(20,0);
 
-	Campeonato c ("Campeonato A", d1, d2);
-*/
-	/*
-	if (c.criaDesportosCampeonato("C:\\Users\\Cláudia Marinho\\Desktop\\Projeto-Aeda\\src\\Desportos.txt"))
+	Campeonato c ("Campeonato A", d1, d2, h1,h2);
+
+
+	if (c.criaDesportosCampeonato("Desportos.txt"))
 		cout<< "correu bem\n";
 	else cout << "correu mal\n";
-	*/
+
 
 	//Hora h1(13,30), h2(13,50), h3(20,00);
 	//cout << (h1 < h2) << (h1 < h3) << (h3 > h2);
@@ -303,7 +331,90 @@ int main(){
 		cout << "nao foi adicionada!!!\n";
 	else cout << "correu mal :(\n";
 */
-	menu();
+	//menu();
+/*
+	Desporto D;
+		D = Desporto("Atletismo", "Segundos", true,8);
+		cout << D.getNome() << endl;
+		cout << D.getPontuacao() << endl;
+		cout << D.isCrescente() << endl;
+		cout << endl;
+
+
+		Modalidade M;
+		M = Modalidade("Corrida de 100 Metros", 13, 35, &D);
+		cout << M.getNome() << endl;
+		cout << M.getDesporto()->getNome() << endl;
+		cout << endl;
+
+		Data d1 (2015,11,1);
+		Data d2 (2015,11,10);
+		Hora ha(8,0);
+		Hora hb(19,30);
+
+		Campeonato c ("Campeonato A", d1, d2,ha,hb);
+
+	Data dat1 (2015,11,1);
+		Hora h1 (13,30);
+
+		Desporto des1("Futebol", "Golos", true,8);
+		Modalidade mod1("Final", 1,30, &des1);
+
+		Prova p1(&mod1,dat1,h1);
+
+		bool resultado1 = c.adicionaProva(p1);
+
+		if (resultado1)
+			cout << "correu bem!\n";
+		else cout << "correu mal :(\n";
+
+		Desporto des2("FutebolAmericano", "Golos", true,2);
+			Modalidade mod2("Meia-Final", 1,30, &des2);
+
+	Desporto des4("Corrida", "Segundos", true,8);
+		Modalidade mod4("500 metros", 1,30, &des4);
+
+		Prova p4(&mod4,dat1,h1);
+
+		bool resultado4 = c.adicionaProva(p4);
+
+		if (resultado4)
+			cout << "foi adicionada, pois provas simultaneas mas de desportos diferentes!\n";
+		else cout << "correu mal :(\n";
+
+	Hora h6(11,59);
+		Desporto des7("Corrida", "Segundos", true,4);
+		Modalidade mod7("500 metros", 1,30, &des7);
+
+		Prova p7(&mod7,dat1,h6);
+
+		bool resultado7 = c.adicionaProva(p7);
+
+		if (resultado7)
+			cout << "foi adicionada!!!\n";
+		else cout << "correu mal :(\n";
+
+		Hora h7(15,01);
+		Desporto des8("Corrida", "Segundos", true,2);
+		Modalidade mod8("500 metros", 1,30, &des8);
+
+		Prova p8(&mod8,dat1,h7);
+
+		bool resultado8 = c.adicionaProva(p8);
+
+		if (resultado8)
+			cout << "foi adicionada!!!\n";
+		else cout << "correu mal :(\n";
+
+
+		c.adicionaDesporto(D);
+		c.adicionaDesporto(des1);
+		c.adicionaDesporto(des2);
+		c.listaDesportos();
+		cout << endl;
+		c.listaProvas();
+*/
+
 	cout << "Press any key to continue...";
 	_getch();
 	return 0;
